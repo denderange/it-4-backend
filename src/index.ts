b3f494a8433cbebd77a3f06e51778d1a47a9f5de
Dennis Polukaroff <dennispolukaroff@gmail.com>
2025-01-26 14:47:52 +0300

    change port

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/api/auth', authRoutes);
 app.use('/api', usersRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
